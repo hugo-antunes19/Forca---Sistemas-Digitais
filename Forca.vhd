a866529382acd98300c1125d1fe728efe1027a75
Giovanni Faletti Almeida <130237551+Giouw1@users.noreply.github.com>
2023-12-12 15:50:06 -0300

    Update Forca.vhd

--- Forca.vhd
+++ Forca.vhd
@@ -8,13 +8,13 @@ use IEEE.numeric_std.all;
 
 entity FORCA2 is
 Port (
-		chute: in std_logic_vector (2 downto 0);
+		chute: in std_logic_vector (2 downto 0); -- VETOR QUE SIMBOLIZA O CHUTE, CADA SWITCH, PODENDO SER 0 OU 1, REPRESENTA 1 BIT DO CHUTE
 		reset : in std_logic := '0';
 		clk: in std_logic;
-		estado_out: out std_logic_vector (4 downto 0);
-		botao: in std_logic := '0';   -- FUNCIONA COMO CLOCK
+		estado_out: out std_logic_vector (4 downto 0); -- VETOR QUE SIMBOLIZA CASO O PRIMEIRO(7), SEGUNDO(6), ATE O QUINTO DIGITOS JA FORAM ACERTADOS, DA ESQUERDA PARA A DIREITA
+		botao: in std_logic := '0';   -- FUNCIONA COMO CLOCK, O CÓDIGO SÓ "FUNCIONA" COM O APERTAR DO BOTAO
 		vidas: out std_logic_vector (2 downto 0) := "111";
-		vitoria: out std_logic; -- VITRIA
+		vitoria: out std_logic; -- VITORIA
 		derrota: out std_logic);  -- DERROTA
 		
 -- SENHA = 71650
@@ -24,24 +24,29 @@ end FORCA2;
 
 architecture Behavioral of FORCA2 is
 
-signal estado_s : std_logic_vector (4 downto 0) := "00000";
-signal vidas_s : std_logic_vector (2 downto 0) := "111"; 
+signal estado_s : std_logic_vector (4 downto 0) := "00000"; -- COMEÇA SEM NENHUM ACERTO
+signal vidas_s : std_logic_vector (2 downto 0) := "111"; -- COMEÇA COM TODAS AS VIDAS
 signal botao_s : std_logic;
 signal chute_s : std_logic_vector (2 downto 0);
 signal vitoria_s : std_logic := '0';
 signal derrota_s : std_logic:= '0';
-signal erros : std_logic_vector(2 downto 0) := "000";
+signal erros : std_logic_vector(2 downto 0) := "000"; -- O VETOR ERROS SURGIU PARA SOLUCIONAR UM ERRO ASSOCIADO AO BOTÃO, EM QUE A RESPOSTA ERA CONTABILIZADA
+-- DIVERSAS VEZES, RESULTANDO EM UMA DERROTA INSTANTÂNEA, DEVIDO À VELOCIDADE DO CLOCK (DIVERSAS SUBIDAS DE CLOCK ACONTECIAM NO APERTAR DO BOTÃO)
+-- OBJETIVAMENTE COM O O VETOR ERROS, PORTANTO, ESSE ERRO FOI SOLUCIONADO, ALÉM DE CHUTES REPETIDOS SEREM DESCONSIDERADOS, POIS ELE IMPEDE QUE CHUTES
+-- ERRADOS SEJAM FEITOS MAIS DE UMA VEZ.
 
 begin
 
 process (clk,reset) begin
 
 if(clk' event and clk = '1') then
-
+-- NESSE TRECHO, SERÃO COMPARADOS OS CHUTES E OS DÍGITOS DA SENHA, RESULTANDO EM UM ACERTO, COM O BIT CORRESPONDENTE AO DÍGITO NO VETOR ESTADO-S INDO DE O PARA 1,
+-- EM CASO DE ACERTO, E A VIDA INDO DE 1 PARA 0, EM CASO DE ERRO
+-- NOSSO CÓDIGO FUNCIONOU SEM CONTABILIZAR CHUTES REPETIDOS
 
 	case chute is
 
-	when "111" =>
+	when "111" => -- 7, dígito 1 da esquerda pra direita
 		if (reset = '1') then
 			estado_s <= "00000"; 
 			vitoria_s <= '0';
@@ -55,7 +60,7 @@ if(clk' event and clk = '1') then
 			derrota_s <= '0';
 		end if; 
 		
-	when "001" =>
+	when "001" => -- 1, dígito 2 da esquerda pra direita
 		if (reset = '1') then
 			estado_s <= "00000";
 			vitoria_s <= '0';
@@ -68,7 +73,7 @@ if(clk' event and clk = '1') then
 		end if; 
 		
 		
-	when "110" =>
+	when "110" => -- 6, dígito 3 da esquerda pra direita
 		if (reset = '1') then
 			estado_s <= "00000";
 			vitoria_s <= '0';
@@ -81,7 +86,7 @@ if(clk' event and clk = '1') then
 		end if; 
 	
 		
-	when "101" =>
+	when "101" => --5, dígito 4 da esquerda pra direita
 		if (reset = '1') then
 			estado_s <= "00000"; 
 			vitoria_s <= '0';
@@ -94,7 +99,7 @@ if(clk' event and clk = '1') then
 		end if; 
 		
 		
-	when "000" =>
+	when "000" => -- 0, dígito 5 da esquerda pra direita
 		if (reset = '1') then
 			estado_s <= "00000"; 
 			vitoria_s <= '0';
@@ -105,7 +110,7 @@ if(clk' event and clk = '1') then
 		if (botao = '1') then
 			estado_s(0) <= '1';
 		end if; 
-
+	-- DAQUI EM DIANTE, TODOS OS CHUTES ERRADOS SÃO VERIFICADOS
 	when "010" =>
 	if (reset = '1') then
 			estado_s <= "00000";
@@ -115,6 +120,7 @@ if(clk' event and clk = '1') then
 			erros <= "000";
 		end if;
 	if (botao = '1' AND erros(2) = '0') then
+		-- A LÓGICA DE TIRAR VIDAS FOI FEITA DESSA FORMA PARA QUE SE TIRE AS VIDAS NA ORDEM, DA ESQUERDA PARA A DIREITA
 		if vidas_s(2) = '1' then
 			vidas_s(2) <= '0';
 			erros(2) <= '1';
