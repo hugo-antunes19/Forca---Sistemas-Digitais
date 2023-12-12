f60317114d4499e4e9946d7386032f4406805713
Hugo Antunes <140461883+hugo-antunes19@users.noreply.github.com>
2023-12-12 16:21:08 -0300

    Update LCD.vhd

--- LCD.vhd
+++ LCD.vhd
@@ -2,329 +2,329 @@
 Library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;		
 		
-		entity LCD is
-		generic (fclk: natural := 110_000_000); -- 110MHz , cristal do kit EE03
-		port (
-				 botao       : in std_logic := '0';
-				 reset       : in std_logic := '0';
-				 chute       : in std_logic_vector (2 downto 0);
-		       clk         : in std_logic; 
-			RS, RW      : out bit;
-			vidas 		 : out std_logic_vector (2 downto 0);
-		       E           : buffer bit;  
-		       DB          : out bit_vector(7 downto 0)); 
-		end LCD;
+		ENTITY LCD IS
+		GENERIC (fclk: natural := 110_000_000); -- 110MHz , crIStal do kit EE03
+		PORT (
+				 botao       : IN std_logic := '0';
+				 reset       : IN std_logic := '0';
+				 chute       : IN std_logic_vector (2 DOWNTO 0);
+		       clk         : IN std_logic; 
+			RS, RW      : OUT bit;
+			vidas 		 : OUT std_logic_vector (2 DOWNTO 0);
+		       E           : BUFFER bit;  
+		       DB          : OUT bit_vector(7 DOWNTO 0)); 
+		END LCD;
 
 
 
-		architecture hardware of LCD is
+		architecture hardware of LCD IS
 		
-		type state is (FunctionSetl, FunctionSet2, FunctionSet3,
+		TYPE state IS (FunctionSetl, FunctionSet2, FunctionSet3,
 		 FunctionSet4,FunctionSet5,FunctionSet6,FunctionSet7,FunctionSet8,FunctionSet9,FunctionSet10,FunctionSet11,FunctionSet12,
-		 FunctionSet13,FunctionSet14,FunctionSet15,FunctionSet16,FunctionSet17,FunctionSet18,FunctionSet19,ClearDisplay, DisplayControl, EntryMode, 
+		 FunctionSet13,FunctionSet14,FunctionSet15,FunctionSet16,FunctionSet17,FunctionSet18,FunctionSet19,ClearDISplay, DISplayControl, EntryMode, 
 		 WriteData1, WriteData2, WriteData3, WriteData4, WriteData5,WriteData6,WriteData7,WriteData8,WriteData9,WriteData10,WriteData11,
 		 WriteData12,SetAddress1,SetAddress2, ReturnHome);
 		 
-		signal ee : std_logic := '0';
-		signal pr_state, nx_state: state;
-		signal estado_out: std_logic_vector (4 downto 0);
-		signal vitoria : std_logic:= '0';
-		signal derrota : std_logic:= '0';
-		signal vidas_f : std_logic_vector (2 downto 0);
+		SIGNAL ee : std_logic := '0';
+		SIGNAL pr_state, nx_state: state;
+		SIGNAL estado_out: std_logic_vector (4 DOWNTO 0);
+		SIGNAL vitoria : std_logic:= '0';
+		SIGNAL derrota : std_logic:= '0';
+		SIGNAL vidas_f : std_logic_vector (2 DOWNTO 0);
 		
 ----Componentes :---------
-	component Forca is
-		Port (
-			tentativa: in std_logic_vector (2 downto 0); -- VETOR QUE SIMBOLIZA O CHUTE, CADA SWITCH, PODENDO SER 0 OU 1, REPRESENTA 1 BIT DO CHUTE
-			reset : in std_logic := '0';
-			clk: in std_logic;
-			estado_out: out std_logic_vector (4 downto 0); -- VETOR QUE SIMBOLIZA CASO O PRIMEIRO(7), SEGUNDO(6), ATE O QUINTO DIGITOS JA FORAM ACERTADOS, DA ESQUERDA PARA A DIREITA
-			botao: in std_logic := '0';   -- FUNCIONA COMO CLOCK, O CÓDIGO SÓ "FUNCIONA" COM O APERTAR DO BOTAO
-			vida: out std_logic_vector (2 downto 0) := "111";
-			ganha: out std_logic; -- VITORIA
-			perde: out std_logic);  -- DERROTA
-	end component;
+	COMPONENT Forca IS
+		PORT (
+			tentativa: IN std_logic_vector (2 DOWNTO 0); -- VETOR QUE SIMBOLIZA O CHUTE, CADA SWITCH, PODENDO SER 0 OU 1, REPRESENTA 1 BIT DO CHUTE
+			reset : IN std_logic := '0';
+			clk: IN std_logic;
+			estado_out: OUT std_logic_vector (4 DOWNTO 0); -- VETOR QUE SIMBOLIZA CASO O PRIMEIRO(7), SEGUNDO(6), ATE O QUINTO DIGITOS JA FORAM ACERTADOS, DA ESQUERDA PARA A DIREITA
+			botao: IN std_logic := '0';   -- FUNCIONA COMO CLOCK, O CÓDIGO SÓ "FUNCIONA" COM O APERTAR DO BOTAO
+			vida: OUT std_logic_vector (2 DOWNTO 0) := "111";
+			ganha: OUT std_logic; -- VITORIA
+			perde: OUT std_logic);  -- DERROTA
+	END COMPONENT;
 	
-begin 	
+BEGIN 	
 							
 ----Modulo da Forca :---------
-forca_f: Forca port map (chute, reset, ee, estado_out, botao, vidas_f, vitoria, derrota);	
+forca_f: Forca PORT MAP (chute, reset, ee, estado_out, botao, vidas_f, vitoria, derrota);	
 vidas <= vidas_f;
 ----Clock generator (E->500Hz) :---------
-		process (clk)
-		variable count: natural range 0 to fclk/100; 
-		begin
-			if (clk' event and clk = '1') then 
+		PROCESS (clk)
+		VARIABLE count: natural range 0 to fclk/100; 
+		BEGIN
+			IF (clk' event AND clk = '1') THEN 
 				count := count + 1;
-				if (count=fclk/100) then 
+				IF (count=fclk/100) THEN 
 					ee <= not ee;
 					E <= not E; 
 					count := 0; 
-				end if; 
-			end if; 
-	end process;
+				END IF; 
+			END IF; 
+	END PROCESS;
 		
 -----Lower section of FSM:---------------
-		process (E) 
-		begin
-			if (E' event and E = '1') then 
+		PROCESS (E) 
+		BEGIN
+			IF (E' event AND E = '1') THEN 
 			--	IF (rst= '0') THEN
 				pr_state <= FunctionSetl; 
 		--ELSE
 		pr_state <= nx_state; 
 		--END IF; 
-		end if; 
-		end process;
+		END IF; 
+		END PROCESS;
 		
 -----Upper section of FSX:---------------
-		process (pr_state) 
-		begin
-		case pr_state is
+		PROCESS (pr_state) 
+		BEGIN
+		CASE pr_state IS
 
 
-		when FunctionSetl => 
+		WHEN FunctionSetl => 
 		RS<= '0'; RW<= '0'; 
 		DB<= "00111000"; 
 		nx_state <= FunctionSet2;
 		
-		when FunctionSet2 => 
+		WHEN FunctionSet2 => 
 		RS<= '0'; RW<= '0'; 
 		DB <= "00111000";
 
 		nx_state <= FunctionSet3; 
-		when FunctionSet3 => 
+		WHEN FunctionSet3 => 
 		RS <= '0'; RW<='0'; 
 		DB <= "00111000"; 
 		nx_state <= FunctionSet4;
 
-		when   FunctionSet4   =>
+		WHEN   FunctionSet4   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet5;
 
-		when   FunctionSet5   =>
+		WHEN   FunctionSet5   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet6;
 
-		when   FunctionSet6   =>
+		WHEN   FunctionSet6   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet7;
 
-		when   FunctionSet7   =>
+		WHEN   FunctionSet7   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet8;
 
-		when   FunctionSet8   =>
+		WHEN   FunctionSet8   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet9;
 
-		when   FunctionSet9   =>
+		WHEN   FunctionSet9   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet10;
 
-		when   FunctionSet10   =>
+		WHEN   FunctionSet10   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet11;
 
-		when   FunctionSet11   =>
+		WHEN   FunctionSet11   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet12;
 
-		when   FunctionSet12   =>
+		WHEN   FunctionSet12   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet13;
 
-		when   FunctionSet13   =>
+		WHEN   FunctionSet13   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet14;
 
-		when   FunctionSet14   =>
+		WHEN   FunctionSet14   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet15;
 
-		when   FunctionSet15   =>
+		WHEN   FunctionSet15   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet16;
 
-		when   FunctionSet16   =>
+		WHEN   FunctionSet16   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet17;
 
-		when   FunctionSet17   =>
+		WHEN   FunctionSet17   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet18;
 
-		when   FunctionSet18   =>
+		WHEN   FunctionSet18   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
 		nx_state <= FunctionSet19;
 
-		when   FunctionSet19   =>
+		WHEN   FunctionSet19   =>
 		RS<=  '0'; RW<=  '0';
 		DB   <=   "00111000";
-		nx_state <= ClearDisplay ;
+		nx_state <= ClearDISplay ;
 
-		when ClearDisplay =>
+		WHEN ClearDISplay =>
 		RS<= '0'; RW<= '0';
 		DB <= "00000001";
-		nx_state   <=   DisplayControl; 
+		nx_state   <=   DISplayControl; 
 
-		when   DisplayControl   =>
+		WHEN   DISplayControl   =>
 		RS<= '0';   RW<=  '0';
 		DB   <=  "00001100";
 		nx_state <= EntryMode; 
 
-		when EntryMode =>
+		WHEN EntryMode =>
 		RS<= '0'; RW<= '0';
 		DB <= "00000110";
 		nx_state   <=  WriteData1; 
 
-		when WriteData1 =>
+		WHEN WriteData1 =>
 		RS<=   '1';   RW <='0';
 		DB   <=   "00100000";   --'ESCREVE UM ESPAO EM BRANCO
 		nx_state <= SetAddress1; 
 
-		when SetAddress1 =>
+		WHEN SetAddress1 =>
 		RS<=   '0';   RW<=   '0';
 		DB   <=  "10000100";   --COMANDO PARA POSICIONAR O CURSOR NA LINHA 0 COLUNA 5
 		nx_state  <= WriteData2; 
 		
-		when WriteData2 =>
+		WHEN WriteData2 =>
 		RS<=   '1';   RW <='0';
-		if (estado_out(4) = '1') then
+		IF (estado_out(4) = '1') THEN
 			DB <= X"37"; -- 7
-		else
+		ELSE
 			DB <= X"5F"; -- "-"
-		end if;
+		END IF;
 		nx_state <= WriteData3; 
 		
-		when WriteData3 =>
+		WHEN WriteData3 =>
 		RS<=   '1';   RW <='0';
-		if (estado_out(3) = '1') then
+		IF (estado_out(3) = '1') THEN
 			DB <= X"31"; -- 1
-		else
+		ELSE
 			DB <= X"5F"; -- "-"
-		end if;
+		END IF;
 		nx_state <= WriteData4; 
 		
-		when WriteData4 =>
+		WHEN WriteData4 =>
 		RS<=   '1';   RW <='0';
-		if (estado_out(2) = '1') then
+		IF (estado_out(2) = '1') THEN
 			DB <= X"36"; -- 6
-		else
+		ELSE
 			DB <= X"5F"; -- "-"
-		end if;
+		END IF;
 		nx_state <= WriteData5; 
 		
-		when WriteData5 =>
+		WHEN WriteData5 =>
 		RS<=   '1';   RW <='0';
-		if (estado_out(1) = '1') then
+		IF (estado_out(1) = '1') THEN
 			DB <= X"35"; -- 5
-		else
+		ELSE
 			DB <= X"5F"; -- "-"
-		end if;
+		END IF;
 		nx_state <= WriteData6; 
 		
-		when WriteData6 =>
+		WHEN WriteData6 =>
 		RS<=   '1';   RW <='0';
-		if (estado_out(0) = '1') then
+		IF (estado_out(0) = '1') THEN
 			DB <= X"30"; -- 7
-		else
+		ELSE
 			DB <= X"5F"; -- "-"
-		end if;
+		END IF;
 		nx_state <= SetAddress2; 
 		
-		when SetAddress2 =>
+		WHEN SetAddress2 =>
 		RS<=   '0';   RW<=   '0';
 		DB   <=  "11000101";   --COMANDO PARA POSICIONAR O CURSOR NA LINHA 2 COLUNA 6
 		nx_state  <= WriteData7; 
 		
-		when WriteData7 =>
+		WHEN WriteData7 =>
 		RS<=   '1';   RW <='0';
-		if (vitoria = '1' and derrota = '0') then
+		IF (vitoria = '1' AND derrota = '0') THEN
 			DB <= X"47"; -- G
-		elsif (vitoria = '0' and derrota = '1') then
+		ELSIF (vitoria = '0' AND derrota = '1') THEN
 			DB <= X"50"; -- P
-		else
+		ELSE
 			DB <= X"20"; -- " "
-		end if;		
+		END IF;		
 		nx_state <= WriteData8; 
 		
-		when WriteData8 =>
+		WHEN WriteData8 =>
 		RS<=   '1';   RW <='0';
-		if (vitoria = '1' and derrota = '0') then
+		IF (vitoria = '1' AND derrota = '0') THEN
 			DB <= X"61"; -- a
-		elsif (vitoria = '0' and derrota = '1') then
+		ELSIF (vitoria = '0' AND derrota = '1') THEN
 			DB <= X"65"; -- e
-		else
+		ELSE
 			DB <= X"20"; -- " "
-		end if;		
+		END IF;		
 		nx_state <= WriteData9; 
 		
-		when WriteData9 =>
+		WHEN WriteData9 =>
 		RS<=   '1';   RW <='0';
-		if (vitoria = '1' and derrota = '0') then
+		IF (vitoria = '1' AND derrota = '0') THEN
 			DB <= X"6e"; -- n
-		elsif (vitoria = '0' and derrota = '1') then
+		ELSIF (vitoria = '0' AND derrota = '1') THEN
 			DB <= X"72"; -- r
-		else
+		ELSE
 			DB <= X"20"; -- " "
-		end if;		
+		END IF;		
 		nx_state <= WriteData10; 
 		
-		when WriteData10 =>
+		WHEN WriteData10 =>
 		RS<=   '1';   RW <='0';
-		if (vitoria = '1' and derrota = '0') then
+		IF (vitoria = '1' AND derrota = '0') THEN
 			DB <= X"68"; -- h
-		elsif (vitoria = '0' and derrota = '1') then
+		ELSIF (vitoria = '0' AND derrota = '1') THEN
 			DB <= X"64"; -- d
-		else
+		ELSE
 			DB <= X"20"; -- " "
-		end if;		
+		END IF;		
 		nx_state <= WriteData11; 
 		
-		when WriteData11 =>
+		WHEN WriteData11 =>
 		RS<=   '1';   RW <='0';
-		if (vitoria = '1' and derrota = '0') then
+		IF (vitoria = '1' AND derrota = '0') THEN
 			DB <= X"6f"; -- o
-		elsif (vitoria = '0' and derrota = '1') then
+		ELSIF (vitoria = '0' AND derrota = '1') THEN
 			DB <= X"65"; -- e
-		else
+		ELSE
 			DB <= X"20"; -- " "
-		end if;		
+		END IF;		
 		nx_state <= WriteData12; 
 		
-		when WriteData12 =>
+		WHEN WriteData12 =>
 		RS<=   '1';   RW <='0';
-		if (vitoria = '1' and derrota = '0') then
+		IF (vitoria = '1' AND derrota = '0') THEN
 			DB <= X"75"; -- u
-		elsif (vitoria = '0' and derrota = '1') then
+		ELSIF (vitoria = '0' AND derrota = '1') THEN
 			DB <= X"75"; -- u
-		else
+		ELSE
 			DB <= X"20"; -- " "
-		end if;		
+		END IF;		
 		nx_state <= ReturnHome;
 
-		when ReturnHome =>
+		WHEN ReturnHome =>
 		RS<=   '0';   RW <='0';
 		DB <= "10000000";
 		nx_state <= WriteData1;
 
 
-	end case; 
-	end process;
+	END CASE; 
+	END PROCESS;
 
-	end hardware; 
+	END hardware; 
